5535421649b97b0c791af86147c03e361053cb65
zzangsub <157343988+zzangsub@users.noreply.github.com>
2024-05-10 18:19:05 +0900

    Update metric.py

--- src/metric.py
+++ src/metric.py
@@ -3,7 +3,35 @@ import torch
 
 # [TODO] Implement this!
 class MyF1Score(Metric):
-    pass
+    
+    def __init__(self):
+        super(MyF1Score, self).__init__()
+        self.add_state("tp", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("fp", default=torch.tensor(0), dist_reduce_fx="sum")
+        self.add_state("fn", default=torch.tensor(0), dist_reduce_fx="sum")
+
+    def update(self, preds: torch.Tensor, target: torch.Tensor):
+        # 예측값에서 가장 높은 확률의 인덱스를 찾습니다.
+        preds_max_index = torch.argmax(preds, dim=1)
+
+        # True Positive (정답을 정답이라고 예측)
+        tp = torch.sum((preds_max_index == target) & (target == 1))
+        # False Positive (오답을 정답이라고 예측)
+        fp = torch.sum((preds_max_index != target) & (preds_max_index == 1))
+        # False Negative (정답을 오답이라고 예측)
+        fn = torch.sum((preds_max_index != target) & (target == 1))
+
+        self.tp += tp
+        self.fp += fp
+        self.fn += fn
+
+    def compute(self):
+        # 정밀도와 재현율을 계산
+        precision = self.tp / (self.tp + self.fp + 1e-6)  # 분모가 0이 되는 것을 방지
+        recall = self.tp / (self.tp + self.fn + 1e-6)
+        # F1 Score 계산
+        f1_score = 2 * (precision * recall) / (precision + recall + 1e-6)  # 분모가 0이 되는 것을 방지
+        return f1_score
 
 class MyAccuracy(Metric):
     def __init__(self):
